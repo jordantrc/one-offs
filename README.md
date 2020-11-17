b8fc214cc26e5607e5d231edf7da78a26aabf78c
Jordan Chadwick <jordan.chadwick@gmail.com>
2020-11-17 14:03:37 -0500

    more options for ascii_to_binary.py, hic sunt dracones

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
 # one-offs
 One off scripts, notes, junk, digital wasteland, consider all code untested, not ready for use
+
+```
+        ,     \    /      ,        
+       / \    )\__/(     / \       
+      /   \  (_\  /_)   /   \      
+ ____/_____\__\@  @/___/_____\____ 
+|             |\../|              |
+|              \VV/               |
+|         Here be dragons         |
+|        Hic sunt dracones        |
+|_________________________________|
+ |    /\ /      \\       \ /\    | 
+ |  /   V        ))       V   \  | 
+ |/     `       //        '     \| 
+ `              V                '
+```
--- python/ascii_to_binary.py
+++ python/ascii_to_binary.py
@@ -1,11 +1,19 @@
 #!/usr/bin/env python3
 #
 # Usage:
-# ascii_to_binary.py <ascii text>
-# If not text is provided the program
-# will prompt the user to provide some.
+# ascii_to_binary.py [OPTIONS] <ascii text>
+# Options:
+#   -d, --detail    provide the letter converted
+#                   as well as the binary equivalent
+#                   in the output
+#   -s, --separator use this separator during output
+# If no text is provided the program
+# will prompt the user to provide input.
+# The program only works with the original
+# 7-bit ASCII character set.
 #
 
+import argparse
 import math
 import sys
 
@@ -14,7 +22,7 @@ def char_to_binary(char):
     the most efficient way would involve bit shifting and all that, but I'm lazy."""
     binary = ""
 
-    exponent = 7
+    exponent = 6
     remainder = ord(char)
     while exponent >= 0:
         divisor = int(math.pow(2, exponent))
@@ -28,17 +36,34 @@ def char_to_binary(char):
 
 def main():
     """main function"""
-    if len(sys.argv) == 1:
+    detail = False
+    separator = "\n"
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-d", "--detail", action="store_true", help="provide the character converted in output")
+    parser.add_argument("-s", "--separator", nargs=1, help="use the character as separator")
+    parser.add_argument("input", nargs="?", help="string to convert")
+    args = parser.parse_args()
+
+    if args.detail:
+        detail = True
+
+    if args.separator is not None:
+        separator = args.separator[0]
+
+    if args.input is None:
         # prompt for input
         ascii_input = input("Enter ASCII text: ")
     else:
-        ascii_input = sys.argv[1]
+        ascii_input = args.input
     
     output = ""
     if len(ascii_input) > 0:
         for c in ascii_input:
             binary = char_to_binary(c)
-            output = output + "\n" + binary
+            if detail:
+                output = output + "%s%s - %s" % (separator, c, binary)
+            else:
+                output = output + separator + binary
     
     print("Binary:")
     print(output)
